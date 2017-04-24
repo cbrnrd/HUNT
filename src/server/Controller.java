b1ef8285dc26fbbec6cc0af153b11adce340c7d6
thecarterb <cbawsome77@gmail.com>
2017-04-23 22:20:14 -0400

    Add settings dialog

--- src/server/Controller.java
+++ src/server/Controller.java
@@ -1,20 +1,50 @@
 package server;
 
 import javafx.application.Platform;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Menu;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
+
+
 public class Controller {
+
+    @FXML
+    private GridPane settingsGridPane;
+
+
+
+    public void initialize(){
+
+    }
+
 
     public void onClose(){
         Platform.exit();
     }
 
-    public void onSettings() throws IOException{
-        Parent root = FXMLLoader.load(getClass().getResource("settings.fxml"));
+    public void onSettings(){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("settings.fxml"));
+            Parent root1 =  fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.initStyle(StageStyle.DECORATED);
+            stage.setTitle("HUNT Server Settings");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch (IOException ioe){
 
+        }
     }
 
 }
